c523257dd28e0668dc025b59cc1e0986f86f5ee3
anfejaramillo <anfejaramillo@utp.edu.co>
2022-11-23 19:45:25 -0500

    23-11-2022

--- src/config.js
+++ src/config.js
@@ -1,9 +1,22 @@
-let PORT = 8080;
-let HOST = "localhost";
-let PROTOCOL = "http";
+// let PORT = 8080;
+// let HOST = "localhost";
+// let PROTOCOL = "http";
+// let API_ROUTE = "/api/";
+// let FULL_PATH = PROTOCOL + "://" + HOST + ":" + PORT + "/";
+// let FULL_API_PATH = PROTOCOL + "://" + HOST + ":" + PORT + API_ROUTE;
+
+let PORT = 80;
+let HOST = "backendproyecto-nigth.vercel.app";
+let PROTOCOL = "https";
 let API_ROUTE = "/api/";
 let FULL_PATH = PROTOCOL + "://" + HOST + ":" + PORT + "/";
 let FULL_API_PATH = PROTOCOL + "://" + HOST + ":" + PORT + API_ROUTE;
+
+if (PORT === 80) {
+    FULL_PATH = PROTOCOL + "://" + HOST + "/";
+    FULL_API_PATH = PROTOCOL + "://" + HOST + API_ROUTE;
+}
+// https://backendproyecto-nigth.vercel.app/api/usuarios/all
 
 module.exports = {
     PORT,
